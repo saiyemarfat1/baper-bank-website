29831f1533be6be4f06530a01a19ae2f946352af
Saiyem Arfat <saiyemarfat775@gmail.com>
2023-08-16 00:47:00 +0600

    newobne

--- JS/login.js
+++ JS/login.js
@@ -15,7 +15,7 @@ console.log(email, passowrd);
 // DANGER: DO NOT VERIFY email passowrd on the client site
 // step 4: verify email and password
 if(email === 'baperid@gmail.com'&& passowrd === 'secret'){
-    window.location.href = 'bank.html'
+    windows.location.href = 'bank.html'
 }else{
     console.log('Invalid-user')
 }
